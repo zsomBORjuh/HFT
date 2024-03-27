0f7199ed29eda7c800e4e54bfd7d76488b6c6840
Jzsombor <95342085+Jzsombor@users.noreply.github.com>
2024-03-27 16:17:58 +0100

    empty console project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adj meg egy számot");
+            int szam = int.Parse(Console.ReadLine());
+            for (int i = 0; i < szam; i++)
+            {
+                if (i == szam)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine(i+", ");
+                }
+            }
+        }
+    }
+}
